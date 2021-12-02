0e94c5b3b491a4395eb1cc2efcc20642af55a395
sakamotodd <skmt_dgr@yahoo.co.jp>
2021-12-02 10:57:06 +0900

    firebase-functions追加

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "staff",
+      "x-hasura-allowed-roles": ["staff"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+  // ログイン時にカスタムクレームを追加する処理
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+    await admin.firestore().collection("user_meta").doc(user.uid).create({
+      refreshTime: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
